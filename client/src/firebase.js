edb2a737566642dc678fdceae025bf43c64eb36e
mohamedishack <hishackdeen@gmail.com>
2024-04-07 21:40:53 +0530

    update Header components

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,16 +2,14 @@
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  authDomain: "mern-blog-36a64.firebaseapp.com",
+  projectId: "mern-blog-36a64",
+  storageBucket: "mern-blog-36a64.appspot.com",
+  messagingSenderId: "761848871238",
+  appId: "1:761848871238:web:c9329af85730a942670d47"
 };
 
 // Initialize Firebase
