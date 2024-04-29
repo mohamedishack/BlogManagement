969323b5bee85c46699eb0844eeecb77009d0288
mohamedishack <hishackdeen@gmail.com>
2024-04-29 22:03:12 +0530

    handle signout

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -8,9 +8,12 @@ import {
   // HiChartPie,
 } from "react-icons/hi";
 import { useEffect, useState } from "react";
+import { useDispatch } from 'react-redux';
 import { Link, useLocation } from "react-router-dom";
+import { signoutSuccess } from "../redux/user/userSlice";
 const DashSidebar = () => {
   const location = useLocation();
+  const dispatch = useDispatch();
   const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -19,6 +22,21 @@ const DashSidebar = () => {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Sidebar className="w-full md:wd-56">
       <Sidebar.Items>
@@ -37,6 +55,7 @@ const DashSidebar = () => {
             active
             icon={HiArrowSmRight}
             labelColor="dark"
+            onClick={handleSignout}
           >
             Signout{" "}
           </Sidebar.Item>
